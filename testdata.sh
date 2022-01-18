edf675e2c96eeff59d32e0e7ea9f73f7cd4d1af0
Ubuntu <ubuntu@ip-172-31-8-159.us-east-2.compute.internal>
2022-01-18 06:34:52 +0000

    generate test data

---
+++ testdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+split=10
+find ~ -type f 2>/dev/null | xargs md5sum 2>/dev/null | awk '{print $1}' | sort -u > testdata.lst
+echo YES SET
+tail -n +$(($split + 1)) testdata.lst
+echo TEST SET
+head -n $split testdata.lst
+

fae665992f30c103d33c5cbb911c78dca5eca391
Kali Linux <nicholas.eden.long@gmail.com>
2022-01-19 19:29:09 +0000

    sha1 test

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gcc -D DATA_SIZE_CHAR=32 bfbf.c bloom/src/bloom.c -o bfbf -fopenmp -lm 
-#gcc -D DATA_SIZE_CHAR=40 bfbf.c bloom/src/bloom.c -o bfbf_sha1 -fopenmp -lm 
+gcc -D DATA_SIZE_CHAR=40 bfbf.c bloom/src/bloom.c -o bfbf_sha1 -fopenmp -lm 
---
+++ sha1test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo Initializing
+./build.sh
+rm data/*
+./bfbf_sha1 -i
+echo Done
+
+split=500
+
+date
+echo "Adding $split hashes"
+head -n $split sha1_test_hashes.lst | ./bfbf_sha1 -a
+
+date
+echo Querying contained: Should print Y for all
+head -n $split sha1_test_hashes.lst | ./bfbf_sha1 -q | grep N
+
+date
+echo Querying not contained: Should print N for most
+failedcount=$(tail -n +$(($split + 1)) sha1_test_hashes.lst | ./bfbf_sha1 -q | grep Y | wc -l)
+echo "Failed: $failedcount / $(tail -n +$(($split + 1)) sha1_test_hashes.lst | wc -l)"
+
+
+date
+
+# bottom part
+# tail -n +$(($split + 1)) sha1_test_hashes.lst
+# top part
+# head -n $split sha1_test_hashes.lst

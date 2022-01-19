e0b1647738cfdf2b29b98c5dd1e750665f89373d
Kali Linux <nicholas.eden.long@gmail.com>
2022-01-19 19:41:32 +0000

    organization: move test data

--- tests/bigtest.sh
+++ tests/bigtest.sh
@@ -10,19 +10,19 @@ split=50000
 
 date
 echo Adding
-head -n $split big_dataset.lst | ./bfbf -a
+head -n $split testinput/big_dataset.lst | ./bfbf -a
 
 date
 echo Querying contained: Should print Y for all
-head -n $split big_dataset.lst | ./bfbf -q | grep N
+head -n $split testinput/big_dataset.lst | ./bfbf -q | grep N
 
 date
 echo Querying not contained: Should print N for most
-tail -n +$(($split + 1)) big_dataset.lst | ./bfbf -q | grep Y
+tail -n +$(($split + 1)) testinput/big_dataset.lst | ./bfbf -q | grep Y
 
 date
 
 # bottom part
-# tail -n +$(($split + 1)) big_dataset.lst
+# tail -n +$(($split + 1)) testinput/big_dataset.lst
 # top part
-# head -n $split big_dataset.lst
+# head -n $split testinput/big_dataset.lst
--- tests/sha1test.sh
+++ tests/sha1test.sh
@@ -10,21 +10,21 @@ split=500
 
 date
 echo "Adding $split hashes"
-head -n $split sha1_test_hashes.lst | ./bfbf_sha1 -a
+head -n $split testinput/sha1_test_hashes.lst | ./bfbf_sha1 -a
 
 date
 echo Querying contained: Should print Y for all
-head -n $split sha1_test_hashes.lst | ./bfbf_sha1 -q | grep N
+head -n $split testinput/sha1_test_hashes.lst | ./bfbf_sha1 -q | grep N
 
 date
 echo Querying not contained: Should print N for most
-failedcount=$(tail -n +$(($split + 1)) sha1_test_hashes.lst | ./bfbf_sha1 -q | grep Y | wc -l)
-echo "Failed: $failedcount / $(tail -n +$(($split + 1)) sha1_test_hashes.lst | wc -l)"
+failedcount=$(tail -n +$(($split + 1)) testinput/sha1_test_hashes.lst | ./bfbf_sha1 -q | grep Y | wc -l)
+echo "Failed: $failedcount / $(tail -n +$(($split + 1)) testinput/sha1_test_hashes.lst | wc -l)"
 
 
 date
 
 # bottom part
-# tail -n +$(($split + 1)) sha1_test_hashes.lst
+# tail -n +$(($split + 1)) testinput/sha1_test_hashes.lst
 # top part
-# head -n $split sha1_test_hashes.lst
+# head -n $split testinput/sha1_test_hashes.lst
